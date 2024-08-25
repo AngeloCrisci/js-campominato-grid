be99af611da7b3e8341304b9284b9014a763fc37
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-25 21:07:11 +0200

    Milestone 3 Da fixare

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 console.log('JS OK')
 
+// * #MILESTONE 2
+// Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+
+//* FUNZIONI
+
+function createGrid(content) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+    grid.append(content);
+    return grid;
+}
+
+//* FASE DI PREPARAZIONE
+// Recupero gli elementi
+
+const button = document.getElementById('play-button')
+const grids = document.getElementById('grids')
+
+//* IMPOSTAZIONI INIZIALI
+
+const rows = 9;
+const cols = 9;
+const totalGrids = rows + cols;
+
+// Aggiungo Button più creazioni delle griglie
+
+button.addEventListener('click' , function (){
+    // ! Blocco l'evento 
+    event.preventDefault();
+
+    // Creo le celle
+    
+    for( let i = 0; i < totalGrids; i++){
+        const grid = createGrid(i + 1);
+        grids.appendChild(grid);
+    }
+})
